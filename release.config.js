0b0d859695f8a06feca539e76cee1dda200f155b
Johan Quiroga <johan@onfleet.com>
2024-10-17 16:27:33 -0500

    chore(release): update release flow

--- release.config.js
+++ release.config.js
@@ -16,16 +16,5 @@ module.exports = {
         failComment: false,
       },
     ],
-    [
-      "@semantic-release/git",
-      {
-        "assets": [
-          "package.json"
-        ],
-        message:
-          // eslint-disable-next-line no-template-curly-in-string
-          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ],
 };
